9619364e17e35941f820d141ce16880706215a42
Saar Agrawal <97218268+DharmaWarrior@users.noreply.github.com>
2023-07-08 03:10:14 +0530

    Update index.js
    Updated API Key

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import {Configuration, OpenAIApi} from 'openai'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY
+  apiKey: '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'
 })
 
 const openai = new OpenAIApi(configuration)
